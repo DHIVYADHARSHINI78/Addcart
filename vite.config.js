222b7bb118bc40c79a54ea1df6c9b46e96a9ada7
DHIVYADHARSHINI78 <dhivyak463@gmail.com>
2025-05-20 11:32:59 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
+  base: '/Addcartusingreact/', // 👈 must match repo name exactly
   plugins: [react()],
-})
+});
+
